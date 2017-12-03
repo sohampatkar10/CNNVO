6a2b8edadb37170263f7ca0502bcc19a8be710a7
soham <spatkar1@jhu.edu>
2017-12-02 23:48:38 -0500

    tf is labels(0,7 and 0,20) and not actual values(-3,3 and -30,30)

--- pretrainparser.py
+++ pretrainparser.py
@@ -13,15 +13,36 @@ class PreTrainParser():
     
     def __getitem__(self, idx):
         im1 = np.array(mnist[0][0])
-        dx = int(6.0*np.random.random() - 3.0)
-        dy = int(6.0*np.random.random() - 3.0)
-        th = 60.0*np.random.random() - 30.0
+
+        # Generate transform
+        rx = np.random.random()
+        ry = np.random.random()
+        rt = np.random.random()
+
+        dx = int(6.0*rx - 3.0)
+        dy = int(6.0*ry - 3.0)
+        th = 60.0*rt - 30.0
 
         tf = transform.SimilarityTransform(scale=1, rotation=0.0, translation=(dx, dy))
 
+        # Transform image
         im2 = transform.warp(im1, tf)
         im2 = transform.rotate(im2, th)
-        
-        dict = {"img1": im1, "img2": im2, "tf":(dx, dy, th)}
-        
+
+        #  Bin transform into classes
+        NUM_XY_CLASSES = 7
+        NUM_TH_CLASSES = 20
+
+        for xy in range(NUM_XY_CLASSES):
+            if (xy - 0.5 <= rx*6.0 < xy + 0.5):
+                lx = xy
+            if (xy - 0.5 <= ry*6.0 < xy + 0.5):
+                ly = xy
+               
+        for t in range(NUM_TH_CLASSES):
+            if (t*3.0 - 1.5 <= rt*60.0 < t*3.0 + 1.5):
+                lt = t
+
+        dict = {"img1": im1, "img2": im2, "tf":(lx, ly, lt)}
+
         return dict
