e05b6041fe212e8851a9ac8fca3aef0cb9c3aa44
soham <spatkar1@jhu.edu>
2017-12-14 17:13:03 -0500

    added .cuda in clas train

--- class_train.py
+++ class_train.py
@@ -6,13 +6,14 @@ class BCNN(torch.nn.Module):
     def __init__(self, pretrain_model = "model1_pretrain.pt"):
         super(BCNN, self).__init__()
         self.pretrain = PreTrain()
-        self.pretrain.load_state_dict(torch.load(pretrain_model)) 
-        self.conv3 = torch.nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1)
+        self.pretrain.load_state_dict(torch.load(pretrain_model))
+        self.pretrain = self.pretrain.cuda()
+        self.conv3 = torch.nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1).cuda()
         
-        self.conv4 = torch.nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1)
+        self.conv4 = torch.nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1).cuda()
         
-        self.conv5 = torch.nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1)
-        self.max5 = torch.nn.MaxPool2d((3,3), stride=[2,2])
+        self.conv5 = torch.nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1).cuda()
+        self.max5 = torch.nn.MaxPool2d((3,3), stride=[2,2]).cuda()
         
         
     def forward(self, x):
@@ -28,11 +29,11 @@ class BCNN(torch.nn.Module):
 class TCNN(torch.nn.Module):
     def __init__(self):
         super(TCNN, self).__init__()
-        self.conv1 = torch.nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=0)
-        self.conv2 = torch.nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=0)
-        self.drop = torch.nn.Dropout(p=0.5)
-        self.linear1 = torch.nn.Linear(50*104*128, 500)
-        self.linear2 = torch.nn.Linear(500, 3)
+        self.conv1 = torch.nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=0).cuda()
+        self.conv2 = torch.nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=0).cuda()
+        self.drop = torch.nn.Dropout(p=0.5).cuda()
+        self.linear1 = torch.nn.Linear(50*104*128, 500).cuda()
+        self.linear2 = torch.nn.Linear(500, 3).cuda()
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
